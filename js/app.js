01f6a4180150de3b9e343f2fcf5fa436d26bbc36
Nicholas Paro <nichparo@amazon.com>
2019-06-18 11:32:44 -0700

    added additional .js files for easier manipulation

--- js/app.js
+++ js/app.js
@@ -1,117 +1,31 @@
 'use strict';
 
 // Global Variables
-var numOfImagesOnPage = 8;
-
+var numOfImagesOnPage = 3;
 var totalCLicks = 0;
+var maxClicks = 25;
 
 // temp storage for images on page - allows for checking against previously shown images
 // in function pickImage().
-var tempArray = [];
-var itemArray = [];
-var resultUlTag = document.getElementById('clickResults');
-var imageSectionTag = document.getElementById('centerBox');
+var previousItemArray = [];
+var currentItemArray = [];
+
+// Global DOM elements - import from dom_manipulation.js
+var resultUlTag = getUlIdTag('clickResults');
+var imageSectionTag = getSectionIdTag('centerBox');
 
 // Constructor
-function ItemPicture(name, imageSrc){
+function ItemPicture(name, imageSrc, timesClicked, timesShown){
   this.name = name;
-  this.clicks = 0;
-  this.timesShown = 0;
-  this.url = imageSrc;
+  this.url = imageSrc ? imageSrc : 'https://placehold.it/220x300/111';
+  this.timesClicked = timesClicked ? timesClicked : 0;
+  this.timesShown = timesShown ? timesShown : 0;
 
   ItemPicture.allImages.push(this);
 }
 ItemPicture.allImages = [];
 
-// Random Number Generator
-function randomNumGen(min, max){
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function indexGenerator(){
-  var indexGen = randomNumGen(0, ItemPicture.allImages.length - 1);
-  return indexGen;
-}
-
-// DOM
-function setDivTag(){
-  var divTag = document.createElement('div');
-  return divTag;
-}
-
-function setImageTag(){
-  var imageTag = document.createElement('img');
-  return imageTag;
-}
-
-function setImageH3(){
-  var imageH3 = document.createElement('h3');
-  return imageH3;
-}
-
-// Rendering Functions
-function renderNewImage(imageIndex){
-  var divTag = setDivTag();
-  var imageTag = setImageTag();
-  var imageH3 = setImageH3();
-  imageTag.src = ItemPicture.allImages[imageIndex].url;
-  imageTag.setAttribute('id', imageIndex);
-  divTag.appendChild(imageTag);
-  imageH3.textContent = ItemPicture.allImages[imageIndex].name;
-  divTag.appendChild(imageH3);
-  imageSectionTag.appendChild(divTag);
-}
-
-function renderResultsList(){
-  var clickPercent = 0;
-  for(let i = 0; i < ItemPicture.allImages.length; i++){
-    var liEl = document.createElement('li');
-    if(ItemPicture.allImages[i].clicks === 0 && ItemPicture.allImages[i].timesShown === 0){
-      clickPercent = 0;
-    } else{
-      clickPercent = (ItemPicture.allImages[i].clicks / ItemPicture.allImages[i].timesShown) * 100;
-    }
-    clickPercent = clickPercent.toFixed(2);
-    liEl.textContent = ItemPicture.allImages[i].name + ': ' + clickPercent + '%';
-    resultUlTag.appendChild(liEl);
-  }
-}
-
-// Handle Functions
-function pickImage(numImages){
-  for(let i=0; i<numImages; i++){
-    do {
-      var imageIndex = indexGenerator();
-    } while(tempArray.includes(imageIndex) || itemArray.includes(imageIndex));
-    itemArray.push(imageIndex);
-    ItemPicture.allImages[imageIndex].timesShown++;
-    renderNewImage(imageIndex);
-  }
-  tempArray = itemArray;
-  itemArray = [];
-
-}
-
-function handleClickOnImage(e){
-  var idNum = e.target.id;
-  if(ItemPicture.allImages[idNum]){
-    ItemPicture.allImages[idNum].clicks++;
-    imageSectionTag.innerHTML = '';
-    pickImage(numOfImagesOnPage);
-    totalCLicks++;
-    if(totalCLicks === 25){
-      imageSectionTag.removeEventListener('click', handleClickOnImage);
-      renderResultsList();
-    }
-  }
-}
-
-/*
-APP INITIALIZER
-*/
-function startApp(){
+function buildItemPicture(){
   new ItemPicture('Bag', './img/bag.jpg');
   new ItemPicture('Banana', './img/banana.jpg');
   new ItemPicture('Bathroom', './img/bathroom.jpg');
@@ -132,9 +46,77 @@ function startApp(){
   new ItemPicture('USB', './img/usb.gif');
   new ItemPicture('Water Can', './img/water-can.jpg');
   new ItemPicture('Wine Glass', './img/wine-glass.jpg');
+}
 
+function indexGenerator(){
+  var indexGen = randomInclusiveNumGen(0, ItemPicture.allImages.length - 1);
+  return indexGen;
+}
+
+// Rendering Functions
+function renderImage(imageIndex){
+  var divTag = setDivTag();
+  var imageTag = setImageTag();
+  var imageH3 = setH3Tag();
+
+  imageTag.src = ItemPicture.allImages[imageIndex].url;
+  imageTag.setAttribute('id', imageIndex);
+  divTag.appendChild(imageTag);
+  imageH3.textContent = ItemPicture.allImages[imageIndex].name;
+  divTag.appendChild(imageH3);
+  imageSectionTag.appendChild(divTag);
+}
+
+function renderResultsList(){
+  var clickPercent, liEl;
+  for(let i = 0; i < ItemPicture.allImages.length; i++){
+    liEl = document.createElement('li');
+    if(ItemPicture.allImages[i].timesClicked === 0 && ItemPicture.allImages[i].timesShown === 0){
+      clickPercent = 0;
+    } else{
+      clickPercent = (ItemPicture.allImages[i].timesClicked / ItemPicture.allImages[i].timesShown) * 100;
+    }
+    clickPercent = clickPercent.toFixed(2);
+    liEl.textContent = ItemPicture.allImages[i].name + ' : ' + clickPercent + '%';
+    resultUlTag.appendChild(liEl);
+  }
+}
+
+// Handle Functions
+function pickImage(numImages){
+  for(let i=0; i<numImages; i++){
+    do {
+      var imageIndex = indexGenerator();
+    } while(previousItemArray.includes(imageIndex) || currentItemArray.includes(imageIndex));
+    currentItemArray.push(imageIndex);
+    ItemPicture.allImages[imageIndex].timesShown++;
+    renderImage(imageIndex);
+  }
+  previousItemArray = currentItemArray;
+  currentItemArray = [];
+}
+
+function handleClickOnImage(e){
+  var idNum = e.target.id;
+  // handles if no image is clicked, only increments if image is clicked
+  if(ItemPicture.allImages[idNum]){
+    ItemPicture.allImages[idNum].timesClicked++;
+    imageSectionTag.innerHTML = '';
+    pickImage(numOfImagesOnPage);
+    totalCLicks++;
+    if(totalCLicks === maxClicks){
+      imageSectionTag.removeEventListener('click', handleClickOnImage);
+      imageSectionTag.innerHTML = ''; // clears the images from the HTML section to allow new item to be placed
+      renderResultsList();
+    }
+  }
+}
+
+// APP INITIALIZER
+function initPage(){
+  buildItemPicture();
   imageSectionTag.addEventListener('click', handleClickOnImage);
   pickImage(numOfImagesOnPage);
 }
 
-startApp();
+initPage();
---
+++ js/dom_manipulation.js
@@ -0,0 +1,31 @@
+// getter tags
+function getSectionIdTag(idName){
+  var sectionIdTag = document.getElementById(idName);
+  return sectionIdTag;
+}
+
+function getUlIdTag(idName){
+  var ulIdTag = document.getElementById(idName);
+  return ulIdTag;
+}
+
+function getChartIdTag(idName){
+  var chartIdTag = document.getElementById(idName);
+  return chartIdTag;
+}
+
+// setter tags
+function setDivTag(){
+  var divTag = document.createElement('div');
+  return divTag;
+}
+
+function setImageTag(){
+  var imageTag = document.createElement('img');
+  return imageTag;
+}
+
+function setH3Tag(){
+  var imageH3 = document.createElement('h3');
+  return imageH3;
+}
